6edc7fc09adb08157305c2d70246841a58e05df3
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-04 01:17:32 +0000

    countLetters.js added

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`💯💯💯 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (string) {
+  const results = {};
+  for (const letter of string) {
+    if (results[letter]) {
+      results[letter] += 1
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
