a74353032644f0bfb415c5755e04f82cfb37ef04
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-08 17:38:10 +0000

    assertEqual.js and head.js  assertions were moved to test directory

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1,4);
-assertEqual("BBB", "BBB");
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,17 +1,7 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(array) {
   return array[0];
 }
-console.log(head([5,4,67]));
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💯💯💯 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+module.exports = head;
