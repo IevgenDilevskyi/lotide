7aa998016c6c69042fd74845b405954b4172df99
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-08 20:32:28 +0000

    assertArraysEqual.js - deleted tests; test/eqArraysEqualTest.js added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arr1, arr2){
-  if (arr1.length !== arr2.length) {
-    return false
-  }
-  for (let i = 0; i < arr1.length; i++){
-    if (arr1[i] !== arr2[i]){
-      return false
-    }
-  } 
-
-  return true
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arrOne, arrTwo) {
   if (eqArrays(arrOne, arrTwo) === true) {
@@ -18,6 +7,5 @@ const assertArraysEqual = function(arrOne, arrTwo) {
     console.log(`😭😭😭 Assertion Failed: ${arrOne} !== ${arrTwo}`);
   }
 }
-assertArraysEqual(["1", "2", "3"], ["1", "2"])
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])
-assertArraysEqual([], [])
+
+module.exports = assertArraysEqual;
